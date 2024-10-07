951bfa8456599fecc6b0f2343ce81ef836267dd7
phantrananhnguyen <22520985@gm.uit.edu.vn>
2024-10-07 16:16:08 +0700

    playfair

---
+++ Lab1/PlayFair/PlayFair/PlayFair/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace PlayFair
+{
+    partial class PlayFair
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridMatrix = new System.Windows.Forms.DataGridView();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.Label();
+            this.Key = new System.Windows.Forms.Label();
+            this.Output = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridMatrix
+            // 
+            this.dataGridMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridMatrix.Location = new System.Drawing.Point(362, 68);
+            this.dataGridMatrix.Name = "dataGridMatrix";
+            this.dataGridMatrix.RowHeadersWidth = 30;
+            this.dataGridMatrix.RowTemplate.Height = 24;
+            this.dataGridMatrix.Size = new System.Drawing.Size(378, 205);
+            this.dataGridMatrix.TabIndex = 0;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(74, 68);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(282, 107);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(74, 227);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(118, 46);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(74, 181);
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(282, 35);
+            this.txtKey.TabIndex = 3;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(74, 295);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(282, 87);
+            this.txtOutput.TabIndex = 4;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(238, 227);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(118, 46);
+            this.btnDecrypt.TabIndex = 5;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // Input
+            // 
+            this.Input.AutoSize = true;
+            this.Input.Location = new System.Drawing.Point(27, 95);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(35, 16);
+            this.Input.TabIndex = 6;
+            this.Input.Text = "Input";
+            // 
+            // Key
+            // 
+            this.Key.AutoSize = true;
+            this.Key.Location = new System.Drawing.Point(27, 189);
+            this.Key.Name = "Key";
+            this.Key.Size = new System.Drawing.Size(30, 16);
+            this.Key.TabIndex = 7;
+            this.Key.Text = "Key";
+            // 
+            // Output
+            // 
+            this.Output.AutoSize = true;
+            this.Output.Location = new System.Drawing.Point(17, 327);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(45, 16);
+            this.Output.TabIndex = 8;
+            this.Output.Text = "Output";
+            // 
+            // PlayFair
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Key);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.dataGridMatrix);
+            this.Name = "PlayFair";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridMatrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridMatrix;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label Input;
+        private System.Windows.Forms.Label Key;
+        private System.Windows.Forms.Label Output;
+    }
+}
+
---
+++ Lab1/PlayFair/PlayFair/PlayFair/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayFair
+{
+    public partial class PlayFair : Form
+    {
+        private char[,] matrix;
+        public PlayFair()
+        {
+            InitializeComponent();
+        }
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string key = txtKey.Text.ToUpper().Replace("J", "I");
+            string plaintext = txtInput.Text.ToUpper().Replace("J", "I");
+            matrix = GenerateMatrix(key);
+            DisplayMatrix();
+            txtOutput.Text = Encrypt(plaintext);
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            string key = txtKey.Text.ToUpper().Replace("J", "I");
+            string ciphertext = txtInput.Text.ToUpper().Replace("J", "I");
+            matrix = GenerateMatrix(key);
+            DisplayMatrix();
+            txtOutput.Text = Decrypt(ciphertext);
+        }
+
+        private char[,] GenerateMatrix(string key)
+        {
+            key = Regex.Replace(key, "[^A-Z]", "");
+            key = new string(key.Distinct().ToArray());
+
+            string alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+            string keyWithAlphabet = key + new string(alphabet.Where(c => !key.Contains(c)).ToArray());
+
+            char[,] matrix = new char[5, 5];
+            for (int i = 0; i < 25; i++)
+            {
+                matrix[i / 5, i % 5] = keyWithAlphabet[i];
+            }
+            return matrix;
+        }
+
+        private void DisplayMatrix()
+        {
+            dataGridMatrix.ColumnCount = 5;
+            dataGridMatrix.RowCount = 5;
+            foreach (DataGridViewColumn column in dataGridMatrix.Columns)
+            {
+                column.Width = 50;
+               
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    dataGridMatrix[j, i].Value = matrix[i, j];
+                }
+            }
+        }
+
+        private string FormatText(string text)
+        {
+            text = Regex.Replace(text, "[^A-Z]", "");
+            StringBuilder formattedText = new StringBuilder();
+            for (int i = 0; i < text.Length; i += 2)
+            {
+                char char1 = text[i];
+                char char2 = (i + 1 < text.Length) ? text[i + 1] : 'X';
+
+                if (char1 == char2)
+                {
+                    formattedText.Append(char1);
+                    formattedText.Append('X');
+                    i--;
+                }
+                else
+                {
+                    formattedText.Append(char1);
+                    formattedText.Append(char2);
+                }
+            }
+            if (formattedText.Length % 2 != 0)
+                formattedText.Append('X');
+
+            return formattedText.ToString();
+        }
+
+        private string Encrypt(string plaintext)
+        {
+            plaintext = FormatText(plaintext);
+            StringBuilder ciphertext = new StringBuilder();
+
+            for (int i = 0; i < plaintext.Length; i += 2)
+            {
+                (int row1, int col1) = FindPosition(plaintext[i]);
+                (int row2, int col2) = FindPosition(plaintext[i + 1]);
+
+                if (row1 == row2)
+                {
+                    ciphertext.Append(matrix[row1, (col1 + 1) % 5]);
+                    ciphertext.Append(matrix[row2, (col2 + 1) % 5]);
+                }
+                else if (col1 == col2)
+                {
+                    ciphertext.Append(matrix[(row1 + 1) % 5, col1]);
+                    ciphertext.Append(matrix[(row2 + 1) % 5, col2]);
+                }
+                else
+                {
+                    ciphertext.Append(matrix[row1, col2]);
+                    ciphertext.Append(matrix[row2, col1]);
+                }
+            }
+
+            return ciphertext.ToString();
+        }
+
+        private string Decrypt(string ciphertext)
+        {
+            StringBuilder plaintext = new StringBuilder();
+
+            for (int i = 0; i < ciphertext.Length; i += 2)
+            {
+                (int row1, int col1) = FindPosition(ciphertext[i]);
+                (int row2, int col2) = FindPosition(ciphertext[i + 1]);
+
+                if (row1 == row2)
+                {
+                    plaintext.Append(matrix[row1, (col1 + 4) % 5]);
+                    plaintext.Append(matrix[row2, (col2 + 4) % 5]);
+                }
+                else if (col1 == col2)
+                {
+                    plaintext.Append(matrix[(row1 + 4) % 5, col1]);
+                    plaintext.Append(matrix[(row2 + 4) % 5, col2]);
+                }
+                else
+                {
+                    plaintext.Append(matrix[row1, col2]);
+                    plaintext.Append(matrix[row2, col1]);
+                }
+            }
+
+            return plaintext.ToString();
+        }
+
+        private (int, int) FindPosition(char c)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    if (matrix[row, col] == c)
+                        return (row, col);
+                }
+            }
+            return (-1, -1);
+        }
+    }
+}
